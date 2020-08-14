f5477ae88eb249bf71549b14ef9782176feea082
marunbabu <marun5689@gmail.com>
2020-08-14 19:07:23 +0530

    Updates

---
+++ precache-manifest.95ecd101c19c5514f31f2db71547d371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a246c456290bcea6596e1e8acc8181bc",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "44ea710924784d728377",
+    "url": "/ReactJS/static/css/main.c2e81465.chunk.css"
+  },
+  {
+    "revision": "ffab1c987eb8c0764777",
+    "url": "/ReactJS/static/js/2.0c37f65e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.0c37f65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ea710924784d728377",
+    "url": "/ReactJS/static/js/main.bd269735.chunk.js"
+  },
+  {
+    "revision": "1e816f6b12fa3b0ac464",
+    "url": "/ReactJS/static/js/runtime-main.ce39f0c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
